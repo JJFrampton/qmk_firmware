40887ab36fa8337f82ebff434c9d9d5ef1d18996
Joseph Frampton <josephjamesframpton@gmail.com>
2018-08-08 19:18:43 -0600

    finalized yoshida
    JJF

--- keyboards/levinson/keymaps/yoshida/keymap.c
+++ keyboards/levinson/keymaps/yoshida/keymap.c
@@ -5,23 +5,16 @@
 extern keymap_config_t keymap_config;
 
 #define _QWERTY 0
-#define _COLEMAK 1
-#define _DVORAK 2
-#define _NUMB 3
-#define _CODE 4
-#define _SYS 5
-#define _FN4 6
+#define _SYMBOLS 1
+#define _MOVEMENT 2
+#define _NUMPAD 3
 #define _ADJUST 16
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
-  COLEMAK,
-  DVORAK,
-  NUMB,
-  CODE,
-  SYS,
-  FN4,
-  ADJUST,
+  SYMBOLS,
+  MOVEMENT,
+  NUMPAD,
 };
 
 #define KC_ KC_TRNS
@@ -31,111 +24,76 @@ enum custom_keycodes {
 #define KC_CPYW LGUI(LSFT(LCTL(KC_3)))  // Copy whole screen
 #define KC_CAPP LGUI(LSFT(KC_4))        // Capture portion of screen
 #define KC_CPYP LGUI(LSFT(LCTL(KC_4)))  // Copy portion of screen
-#define KC_X1 CODE
-#define KC_X2 NUMB
-#define KC_X3 SYS
-#define KC_X4 MT(MOD_LSFT, KC_ENT)
+
+/* Custom mod layers: */
+#define KC_M1 SYMBOLS
+#define KC_M2 MOVEMENT
+#define KC_M3 NUMPAD
+#define KC_M4 SPC
+#define KC_M5 SPC
+/* Mod layer shifters */
+#define KC_SFTD
+#define KC_SFTU
+
+#define KC_CLPR LSFT(9)
+#define KC_CRPR LSFT(0)
+#define KC_CLCB LSFT(LBRC)
+#define KC_CRCB LSFT(RBRC)
+#define KC_CLSQ LBRC
+#define KC_CRSQ RBRC
+#define KC_CLAB LSFT(COMM)
+#define KC_CRAB LSFT(DOT)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT_kc(
   //,----+----+----+----+----+----.    ,----+----+----+----+----+----.
-     TAB , Q  , W  , E  , R  , T  ,      Y  , U  , I  , O  , P  ,MINS,
+     TAB , Q  , W  , E  , R  , T  ,      Y  , U  , I  , O  , P  ,BSPC,
   //|----+----+----+----+----+----|    |----+----+----+----+----+----|
-     BSPC, A  , S  , D  , F  , G  ,      H  , J  , K  , L  ,SCLN,QUOT,
+     ESC , A  , S  , D  , F  , G  ,      H  , J  , K  , L  ,SCLN,QUOT,
   //|----+----+----+----+----+----|    |----+----+----+----+----+----|
-     LSFT, Z  , X  , C  , V  , B  ,      N  , M  ,COMM,DOT ,SLSH, X4 ,
+     LSFT, Z  , X  , C  , V  , B  ,      N  , M  ,COMM,DOT ,SLSH,RSFT,
   //|----+----+----+----+----+----|    |----+----+----+----+----+----|
-      X3 ,LCTL,LALT,LGUI, X1 ,LALT,     SPC , X2 ,LEFT,DOWN, UP ,RGHT
+     LCTL,LALT,LGUI, M1 , M2 , M3 ,     SPC , M4 , M5 ,SFTD,SFTU,ENT
   //`----+----+----+----+----+----'    `----+----+----+----+----+----'
   ),
 
-  [_COLEMAK] = LAYOUT_kc(
+  [_SYMBOLS] = LAYOUT_kc(
   //,----+----+----+----+----+----.    ,----+----+----+----+----+----.
-     TAB , Q  , W  , F  , P  , G  ,      J  , L  , U  , Y  ,SCLN,MINS,
+      F1 , F2 , F3 , F4 , F5 , F6 ,     AMPR,ASTR,CLPR,CRPR, P  ,BSPC,
   //|----+----+----+----+----+----|    |----+----+----+----+----+----|
-     BSPC, A  , R  , S  , T  , D  ,      H  , N  , E  , I  , O  ,QUOT,
+      F7 , F8 , F9 ,F10 ,F11 ,F12 ,     EXLM, AT ,CLCB,CRCB,SCLN,BSLS,
   //|----+----+----+----+----+----|    |----+----+----+----+----+----|
-     LSFT, Z  , X  , C  , V  , B  ,      K  , M  ,COMM,DOT ,SLSH, X4 ,
+     LSFT, Z  , X  , C  , V  , B  ,     HASH,DLR ,CLSQ,CRSQ,SLSH,RSFT,
   //|----+----+----+----+----+----|    |----+----+----+----+----+----|
-      X3 ,LCTL,LALT,LGUI, X1 ,LALT,     SPC , X2 ,LEFT,DOWN, UP ,RGHT
+     LCTL,LALT,LGUI, M1 , M2 , M3 ,     PERC,CIRC,CLAB,CRAB,SFTU,ENT
   //`----+----+----+----+----+----'    `----+----+----+----+----+----'
   ),
 
-  [_DVORAK] = LAYOUT_kc(
+  [_MOVEMENT] = LAYOUT_kc(
   //,----+----+----+----+----+----.    ,----+----+----+----+----+----.
-     TAB ,QUOT,COMM,DOT , P  , Y  ,      F  , G  , C  , R  , L  ,MINS,
+      1  , 2  , 3  , 4  , 5  , 6  ,      7  , 8  , 9  , 0  ,MINS,EQL 
   //|----+----+----+----+----+----|    |----+----+----+----+----+----|
-     BSPC, A  , O  , E  , U  , I  ,      D  , H  , T  , N  , S  ,SLSH,
+     GRAV,BTN1,MS_L,MS_U,MS_D,MS_R,     RGHT,DOWN, UP ,LEFT,SCLN,BSLS,
   //|----+----+----+----+----+----|    |----+----+----+----+----+----|
-     LSFT,SCLN, Q  , J  , K  , X  ,      B  , M  , W  , V  , Z  , X4 ,
+     LSFT,BTN2,WH_L,WH_U,WH_D,WH_R,     HOME,PGDN,PGUP,END ,SLSH,RSFT,
   //|----+----+----+----+----+----|    |----+----+----+----+----+----|
-      X3 ,LCTL,LALT,LGUI, X1 ,LALT,     SPC , X2 ,LEFT,DOWN, UP ,RGHT
+     LCTL,LALT,LGUI, M1 , M2 , M3 ,     SPC , M4 , M5 ,SFTD,SFTU,ENT
   //`----+----+----+----+----+----'    `----+----+----+----+----+----'
   ),
 
-  [_NUMB] = LAYOUT_kc(
+  [_NUMPAD] = LAYOUT_kc(
   //,----+----+----+----+----+----.    ,----+----+----+----+----+----.
-     TILD,EXLM, AT ,HASH,DLR ,PERC,     CIRC,AMPR,ASTR,LPRN,RPRN,PMNS,
+     GRV , Q  , W  , E  , R  , T  ,      Y  , 7  , 8  , 9  ,PMNS,PSLS,
   //|----+----+----+----+----+----|    |----+----+----+----+----+----|
-     GRV , 1  , 2  , 3  , 4  , 5  ,      6  , 7  , 8  , 9  , 0  ,UNDS,
+     ESC , A  , S  , D  , F  , G  ,      H  , 4  , 5  , 6  ,PPLS,PAST,
   //|----+----+----+----+----+----|    |----+----+----+----+----+----|
-         ,    ,MPRV,MPLY,MNXT,    ,         ,VOLD,VOLU,MUTE,    ,    ,
+     LSFT, Z  , X  , C  , V  , B  ,      N  , 1  , 2  , 3  ,ENT ,BSLS,
   //|----+----+----+----+----+----|    |----+----+----+----+----+----|
-         ,    ,    ,    ,    ,LALT,         ,    ,    ,    ,    ,
+     LCTL,LALT,LGUI, M1 , M2 , M3 ,     SPC , 0  , 0  ,DOT ,ENT ,EQL
   //`----+----+----+----+----+----'    `----+----+----+----+----+----'
-  ),
-
-  [_CODE] = LAYOUT_kc(
-  //,----+------+----+----+----+----.    ,----+----+----+----+----+----.
-     ESC ,      ,    , UP ,    ,    ,     PGUP,HOME,LPRN,RPRN,BSLS,PIPE,
-  //|----+------+----+----+----+----|    |----+----+----+----+----+----|
-     CAPS,SELECT,LEFT,DOWN,RGHT,DEL ,     PGDN, END,LBRC,RBRC,MINS,UNDS,
-  //|----+------+----+----+----+----|    |----+----+----+----+----+----|
-         , UNDO ,CUT ,COPY,PASTE,   ,     LEFT,RGHT,LCBR,RCBR,PLUS,PEQL,
-  //|----+------+----+----+----+----|    |----+----+----+----+----+----|
-         ,      ,    ,    ,    ,LALT,         ,    ,    ,    ,    ,
-  //`----+------+----+----+----+----'    `----+----+----+----+----+----'
-  ),
-
-  [_SYS] = LAYOUT_kc(
-  //,----+----+----+----+----+----.    ,----+----+----+----+----+----.
-     F12 , F1 , F2 , F3 , F4 , F5 ,      F6 , F7 , F8 , F9 ,F10 ,F11 ,
-  //|----+----+----+----+----+----|    |----+----+----+----+----+----|
-         ,    ,    ,    ,    ,    ,         ,    ,    ,    ,    ,    ,
-  //|----+----+----+----+----+----|    |----+----+----+----+----+----|
-         ,    ,    ,    ,    ,    ,         ,    ,    ,    ,    ,    ,
-  //|----+----+----+----+----+----|    |----+----+----+----+----+----|
-         ,    ,    ,    ,    ,    ,         ,    ,    ,    ,    ,
-  //`----+----+----+----+----+----'    `----+----+----+----+----+----'
-  ),
-
-/* Adjust (Lower + Raise)
- * ,-----------------------------------------------------------------------------------.
- * |      | Reset|RGB Tg|RGB Md|Hue Up|Hue Dn|Sat Up|Sat Dn|Val Up|Val Dn|      |      |
- * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      |      |Aud on|Audoff|AGnorm|AGswap|Qwerty|Colemk|Dvorak|      |      |
- * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |      |      |      |      |      |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      |      |      |      |      |
- * `-----------------------------------------------------------------------------------'
- */
-  [_ADJUST] = LAYOUT_ortho_4x12( \
-    _______, RESET  , RGB_TOG, RGB_MOD, RGB_HUD, RGB_HUI, RGB_SAD, RGB_SAI, RGB_VAD, RGB_VAI, _______, _______, \
-    _______, _______, _______, AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  COLEMAK, DVORAK,  _______, _______, \
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
   )
-
-
 };
-
-#ifdef AUDIO_ENABLE
-float tone_qwerty[][2]     = SONG(QWERTY_SOUND);
-float tone_dvorak[][2]     = SONG(DVORAK_SOUND);
-float tone_colemak[][2]    = SONG(COLEMAK_SOUND);
-#endif
 
 void persistent_default_layer_set(uint16_t default_layer) {
   eeconfig_update_default_layer(default_layer);
@@ -153,49 +111,33 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       return false;
       break;
-    case COLEMAK:
+    case SYMBOLS:
       if (record->event.pressed) {
-        #ifdef AUDIO_ENABLE
-          PLAY_SONG(tone_colemak);
-        #endif
-        persistent_default_layer_set(1UL<<_COLEMAK);
-      }
-      return false;
-      break;
-    case DVORAK:
-      if (record->event.pressed) {
-        #ifdef AUDIO_ENABLE
-          PLAY_SONG(tone_dvorak);
-        #endif
-        persistent_default_layer_set(1UL<<_DVORAK);
-      }
-      return false;
-      break;
-    case NUMB:
-      if (record->event.pressed) {
-        layer_on(_NUMB);
-        update_tri_layer(_NUMB, _CODE, _ADJUST);
+        layer_on(_SYMBOLS);
+        update_tri_layer(_SYMBOLS, _CODE, _ADJUST);
       } else {
-        layer_off(_NUMB);
-        update_tri_layer(_NUMB, _CODE, _ADJUST);
+        layer_off(_SYMBOLS);
+        update_tri_layer(_SYMBOLS, _CODE, _ADJUST);
       }
       return false;
       break;
-    case CODE:
+    case MOVEMENT:
       if (record->event.pressed) {
-        layer_on(_CODE);
-        update_tri_layer(_NUMB, _CODE, _ADJUST);
+        layer_on(_MOVEMENT);
+        update_tri_layer(_MOVEMENT, _CODE, _ADJUST);
       } else {
-        layer_off(_CODE);
-        update_tri_layer(_NUMB, _CODE, _ADJUST);
+        layer_off(_MOVEMENT);
+        update_tri_layer(_MOVEMENT, _CODE, _ADJUST);
       }
       return false;
       break;
-    case ADJUST:
+    case NUMPAD:
       if (record->event.pressed) {
-        layer_on(_ADJUST);
+        layer_on(_NUMPAD);
+        update_tri_layer(_NUMPAD, _CODE, _ADJUST);
       } else {
-        layer_off(_ADJUST);
+        layer_off(_NUMPAD);
+        update_tri_layer(_NUMPAD, _CODE, _ADJUST);
       }
       return false;
       break;
