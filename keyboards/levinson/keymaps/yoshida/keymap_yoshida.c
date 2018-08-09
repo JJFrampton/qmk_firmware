056709aa86dea27c09186ee2f11cf1fe4e679ed0
Joseph Frampton <josephjamesframpton@gmail.com>
2018-08-08 19:05:30 -0600

    midway
    JJF

--- keyboards/levinson/keymaps/yoshida/keymap_yoshida.c
+++ keyboards/levinson/keymaps/yoshida/keymap_yoshida.c
@@ -5,22 +5,16 @@
 extern keymap_config_t keymap_config;
 
 #define _QWERTY 0
-#define _COLEMAK 1
-#define _DVORAK 2
-#define _NUMB 3
-#define _CODE 4
-#define _SYS 5
-#define _FN4 6
+#define _SYMBOLS 1
+#define _MOVEMENT 2
+#define _NUMPAD 3
 #define _ADJUST 16
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
-  COLEMAK,
-  DVORAK,
-  NUMB,
-  CODE,
-  SYS,
-  FN4,
+  SYMBOLS,
+  MOVEMENT,
+  NUMPAD,
   ADJUST,
 };
 
@@ -31,46 +25,122 @@ enum custom_keycodes {
 #define KC_CPYW LGUI(LSFT(LCTL(KC_3)))  // Copy whole screen
 #define KC_CAPP LGUI(LSFT(KC_4))        // Capture portion of screen
 #define KC_CPYP LGUI(LSFT(LCTL(KC_4)))  // Copy portion of screen
-#define KC_X1 CODE
-#define KC_X2 NUMB
-#define KC_X3 SYS
-#define KC_X4 MT(MOD_LSFT, KC_ENT)
+
+/* Custom mod layers: */
+#define KC_M1 SYMBOLS
+#define KC_M2 MOVEMENT
+#define KC_M3 NUMPAD
+#define KC_M4 SPC
+#define KC_M5 SPC
+/* Mod layer shifters */
+#define KC_SFTD
+#define KC_SFTU
+
+#define KC_CLPR LSFT(9)
+#define KC_CRPR LSFT(0)
+#define KC_CLCB LSFT(LBRC)
+#define KC_CRCB LSFT(RBRC)
+#define KC_CLSQ LBRC
+#define KC_CRSQ RBRC
+#define KC_CLAB LSFT(COMM)
+#define KC_CRAB LSFT(DOT)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT_kc(
   //,----+----+----+----+----+----.    ,----+----+----+----+----+----.
-     TAB , Q  , W  , E  , R  , T  ,      Y  , U  , I  , O  , P  ,MINS,
+     TAB , Q  , W  , E  , R  , T  ,      Y  , U  , I  , O  , P  ,BSPC,
   //|----+----+----+----+----+----|    |----+----+----+----+----+----|
-     BSPC, A  , S  , D  , F  , G  ,      H  , J  , K  , L  ,SCLN,QUOT,
+     ESC , A  , S  , D  , F  , G  ,      H  , J  , K  , L  ,SCLN,QUOT,
   //|----+----+----+----+----+----|    |----+----+----+----+----+----|
-     LSFT, Z  , X  , C  , V  , B  ,      N  , M  ,COMM,DOT ,SLSH, X4 ,
+     LSFT, Z  , X  , C  , V  , B  ,      N  , M  ,COMM,DOT ,SLSH,RSFT,
   //|----+----+----+----+----+----|    |----+----+----+----+----+----|
-      X3 ,LCTL,LALT,LGUI, X1 ,LALT,     SPC , X2 ,LEFT,DOWN, UP ,RGHT
+     LCTL,LALT,LGUI, M1 , M2 , M3 ,     SPC , M4 , M5 ,SFTD,SFTU,ENT
   //`----+----+----+----+----+----'    `----+----+----+----+----+----'
   ),
 
-  [_COLEMAK] = LAYOUT_kc(
+  [_MOVEMENT] = LAYOUT_kc(
   //,----+----+----+----+----+----.    ,----+----+----+----+----+----.
-     TAB , Q  , W  , F  , P  , G  ,      J  , L  , U  , Y  ,SCLN,MINS,
+     TAB , Q  ,WH_L,WH_U,WH_D,WH_R,     HOME,PGDN,PGUP,END , p  ,DEL
   //|----+----+----+----+----+----|    |----+----+----+----+----+----|
-     BSPC, A  , R  , S  , T  , D  ,      H  , N  , E  , I  , O  ,QUOT,
+     BSPC,BTN1,MS_L,MS_U,MS_D,MS_R,     RGHT,DOWN, UP ,LEFT,SCLN,QUOT,
   //|----+----+----+----+----+----|    |----+----+----+----+----+----|
-     LSFT, Z  , X  , C  , V  , B  ,      K  , M  ,COMM,DOT ,SLSH, X4 ,
+     LSFT,BTN2, X  , C  , V  , B  ,      K  , M  ,COMM,DOT ,SLSH,RSFT,
   //|----+----+----+----+----+----|    |----+----+----+----+----+----|
-      X3 ,LCTL,LALT,LGUI, X1 ,LALT,     SPC , X2 ,LEFT,DOWN, UP ,RGHT
+     LCTL,LALT,LGUI, M1 , M2 , M3 ,     SPC , M4 , M5 ,SFTD,SFTU,ENT
   //`----+----+----+----+----+----'    `----+----+----+----+----+----'
   ),
 
-  [_DVORAK] = LAYOUT_kc(
+  [_MOVEMENT_2] = LAYOUT_kc(
   //,----+----+----+----+----+----.    ,----+----+----+----+----+----.
-     TAB ,QUOT,COMM,DOT , P  , Y  ,      F  , G  , C  , R  , L  ,MINS,
+      1  , 2  , 3  , 4  , 5  , 6  ,      7  , 8  , 9  , 0  ,MINS,EQL 
   //|----+----+----+----+----+----|    |----+----+----+----+----+----|
-     BSPC, A  , O  , E  , U  , I  ,      D  , H  , T  , N  , S  ,SLSH,
+     GRAV,BTN1,MS_L,MS_U,MS_D,MS_R,     RGHT,DOWN, UP ,LEFT,SCLN,BSLS,
   //|----+----+----+----+----+----|    |----+----+----+----+----+----|
-     LSFT,SCLN, Q  , J  , K  , X  ,      B  , M  , W  , V  , Z  , X4 ,
+     LSFT,BTN2,WH_L,WH_U,WH_D,WH_R,     HOME,PGDN,PGUP,END ,SLSH,RSFT,
   //|----+----+----+----+----+----|    |----+----+----+----+----+----|
-      X3 ,LCTL,LALT,LGUI, X1 ,LALT,     SPC , X2 ,LEFT,DOWN, UP ,RGHT
+     LCTL,LALT,LGUI, M1 , M2 , M3 ,     SPC , M4 , M5 ,SFTD,SFTU,ENT
+  //`----+----+----+----+----+----'    `----+----+----+----+----+----'
+  ),
+
+  /* [_DVORAK] = LAYOUT_kc( */
+  [_NUMPAD] = LAYOUT_kc(
+  //,----+----+----+----+----+----.    ,----+----+----+----+----+----.
+     GRV , Q  , W  , E  , R  , T  ,      7  , 8  , 9  ,PMNS,PSLS,EQL
+  //|----+----+----+----+----+----|    |----+----+----+----+----+----|
+     ESC , A  , S  , D  , F  , G  ,      4  , 5  , 6  ,PPLS,PAST,BSLS
+  //|----+----+----+----+----+----|    |----+----+----+----+----+----|
+     LSFT, Z  , X  , C  , V  , B  ,      1  , 2  , 3  ,ENT ,ENT ,RSFT
+  //|----+----+----+----+----+----|    |----+----+----+----+----+----|
+     LCTL,LALT,LGUI, M1 , M2 , M3 ,      0  , 0  ,DOT ,ENT ,ENT ,ENT
+  //`----+----+----+----+----+----'    `----+----+----+----+----+----'
+  ),
+
+  [_NUMPAD_2] = LAYOUT_kc(
+  //,----+----+----+----+----+----.    ,----+----+----+----+----+----.
+     GRV , Q  , W  , E  , R  , T  ,      Y  , 7  , 8  , 9  ,PMNS,PSLS,
+  //|----+----+----+----+----+----|    |----+----+----+----+----+----|
+     ESC , A  , S  , D  , F  , G  ,      H  , 4  , 5  , 6  ,PPLS,PAST,
+  //|----+----+----+----+----+----|    |----+----+----+----+----+----|
+     LSFT, Z  , X  , C  , V  , B  ,      N  , 1  , 2  , 3  ,ENT ,BSLS,
+  //|----+----+----+----+----+----|    |----+----+----+----+----+----|
+     LCTL,LALT,LGUI, M1 , M2 , M3 ,     SPC , 0  , 0  ,DOT ,ENT ,EQL
+  //`----+----+----+----+----+----'    `----+----+----+----+----+----'
+  ),
+
+  [_SYMBOLS] = LAYOUT_kc(
+  //,----+----+----+----+----+----.    ,----+----+----+----+----+----.
+     TAB , Q  , W  , E  , R  , T  ,      Y  ,CLPR,CRPR, O  , P  ,BSPC,
+  //|----+----+----+----+----+----|    |----+----+----+----+----+----|
+     ESC , A  , S  , D  , F  , G  ,      H  ,CLCB,CRCB, L  ,SCLN,QUOT,
+  //|----+----+----+----+----+----|    |----+----+----+----+----+----|
+     LSFT, Z  , X  , C  , V  , B  ,      N  ,CLSQ,CRSQ,DOT ,SLSH,RSFT,
+  //|----+----+----+----+----+----|    |----+----+----+----+----+----|
+     LCTL,LALT,LGUI, M1 , M2 , M3 ,     SPC ,CLAB,CRAB,SFTD,SFTU,ENT
+  //`----+----+----+----+----+----'    `----+----+----+----+----+----'
+  ),
+
+  [_SYMBOLS_2] = LAYOUT_kc(
+  //,----+----+----+----+----+----.    ,----+----+----+----+----+----.
+     EXLM, AT ,HASH,DLR ,PERC,CIRC,     AMPR,ASTR,CLPR,CRPR, P  ,BSPC,
+  //|----+----+----+----+----+----|    |----+----+----+----+----+----|
+     ESC , A  , S  ,EXLM,AT ,HASH ,      H  , J  ,CLCB,CRCB,SCLN,BSLS,
+  //|----+----+----+----+----+----|    |----+----+----+----+----+----|
+     LSFT, Z  , X  , C  , V  , B  ,      N  , M  ,CLSQ,CRSQ,SLSH,RSFT,
+  //|----+----+----+----+----+----|    |----+----+----+----+----+----|
+     LCTL,LALT,LGUI, M1 , M2 , M3 ,     SPC , M4 ,CLAB,CRAB,SFTU,ENT
+  //`----+----+----+----+----+----'    `----+----+----+----+----+----'
+  ),
+
+  [_SYMBOLS_3] = LAYOUT_kc(
+  //,----+----+----+----+----+----.    ,----+----+----+----+----+----.
+      F1 , F2 , F3 , F4 , F5 , F6 ,     AMPR,ASTR,CLPR,CRPR, P  ,BSPC,
+  //|----+----+----+----+----+----|    |----+----+----+----+----+----|
+      F7 , F8 , F9 ,F10 ,F11 ,F12 ,     EXLM, AT ,CLCB,CRCB,SCLN,BSLS,
+  //|----+----+----+----+----+----|    |----+----+----+----+----+----|
+     LSFT, Z  , X  , C  , V  , B  ,     HASH,DLR ,CLSQ,CRSQ,SLSH,RSFT,
+  //|----+----+----+----+----+----|    |----+----+----+----+----+----|
+     LCTL,LALT,LGUI, M1 , M2 , M3 ,     PERC,CIRC,CLAB,CRAB,SFTU,ENT
   //`----+----+----+----+----+----'    `----+----+----+----+----+----'
   ),
 
