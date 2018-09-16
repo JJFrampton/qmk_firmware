f304f402bc81e37197dc9b1d2bacc8501e4093ad
Joseph Frampton <josephjamesframpton@gmail.com>
2018-09-15 18:33:57 -0600

    adding min and eq to symbols layer
    JJF

--- keyboards/levinson/keymaps/yoshida/keymap.c
+++ keyboards/levinson/keymaps/yoshida/keymap.c
@@ -67,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_SYMBOLS] = LAYOUT_kc(
   //,----+----+----+----+----+----.    ,----+----+----+----+----+----.
-      F1 , F2 , F3 , F4 , F5 , F6 ,     AMPR,ASTR,CLPR,CRPR, P  ,BSPC,
+      F1 , F2 , F3 , F4 , F5 , F6 ,     AMPR,ASTR,CLPR,CRPR,MINS,EQL ,
   //|----+----+----+----+----+----|    |----+----+----+----+----+----|
       F7 , F8 , F9 ,F10 ,F11 ,F12 ,     EXLM, AT ,CLCB,CRCB,SCLN,BSLS,
   //|----+----+----+----+----+----|    |----+----+----+----+----+----|
