15de16b79a60dd7f8e2e57a96bbdc7bdb526814d
Joseph Frampton <josephjamesframpton@gmail.com>
2019-01-03 18:03:36 -0700

    addind config from other branch
    JJF

--- keyboards/new_test/config.h
+++ keyboards/new_test/config.h
@@ -24,7 +24,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PRODUCT_ID      0x0000
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    You
-#define PRODUCT         new_test
+#define PRODUCT         p14
 #define DESCRIPTION     A custom keyboard
 
 /* key matrix size */
@@ -41,8 +41,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
  *
 */
-#define MATRIX_ROW_PINS { D0, D5 }
-#define MATRIX_COL_PINS { F1, F0, B0 }
+#define MATRIX_ROW_PINS { B2  B3  B1  B0 }
+#define MATRIX_COL_PINS { D5  C7  C6  D3  D2  D1  D0  F0  B6  F7  F6  F4  F5  F1 }
 #define UNUSED_PINS
 
 /* COL2ROW, ROW2COL, or CUSTOM_MATRIX */
@@ -221,4 +221,3 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define LCD_E_PIN        1            //< pin  for Enable line
 #endif
 */
-
